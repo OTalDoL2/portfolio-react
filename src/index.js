67e9c0f86e997c2d67dcaead22937657b847be18
Lucas M <56313048+OTalDoL2@users.noreply.github.com>
2024-11-14 11:23:10 -0300

    init

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Home from './pages/Home';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
